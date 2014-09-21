79564077fdc22b0d426dc5ee5feb819bbe9fb86c
Oleksandr Tereschenko <Oleksandr@terems-pad.(none)>
2014-09-21 18:54:58 +0300

    minor changes

--- webz-api/src/main/java/org/terems/webz/WebzPathnameException.java
+++ webz-api/src/main/java/org/terems/webz/WebzPathnameException.java
@@ -4,27 +4,22 @@ package org.terems.webz;
 @SuppressWarnings("serial")
 public class WebzPathnameException extends WebzException {
 
-	/** TODO !!! describe !!! **/
 	public WebzPathnameException() {
 		super();
 	}
 
-	/** TODO !!! describe !!! **/
 	public WebzPathnameException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
-	/** TODO !!! describe !!! **/
 	public WebzPathnameException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
-	/** TODO !!! describe !!! **/
 	public WebzPathnameException(String message) {
 		super(message);
 	}
 
-	/** TODO !!! describe !!! **/
 	public WebzPathnameException(Throwable cause) {
 		super(cause);
 	}
