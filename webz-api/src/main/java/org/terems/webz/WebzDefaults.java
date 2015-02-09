9a122ec1636dde1c754eafc73b1a62f65a06a7f4
teremterem <toporok@gmail.com>
2015-02-09 08:55:14 +0200

    changed some default settings

--- webz-api/src/main/java/org/terems/webz/WebzDefaults.java
+++ webz-api/src/main/java/org/terems/webz/WebzDefaults.java
@@ -3,8 +3,8 @@ package org.terems.webz;
 /** TODO !!! describe constants !!! **/
 public class WebzDefaults {
 
-	public static final boolean FS_CACHE_ENABLED = true;
-	public static final boolean USE_METADATA_INFLATABLE_FILES = true;
+	public static final boolean FS_CACHE_ENABLED = false;
+	public static final boolean USE_METADATA_INFLATABLE_FILES = false;
 	public static final String FS_IMPL_CLASS = "org.terems.webz.impl.LocalFileSystemImpl";
 	public static final String FS_CACHE_IMPL_CLASS = "org.terems.webz.impl.cache.ehcache.EhcacheFileSystemCache";
 	public static final int FS_CACHE_PAYLOAD_THRESHOLD_BYTES = 262144;
