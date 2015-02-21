40b37b401ae662ca1f7b524aa6432ae06b9ad55f
teremterem <toporok@gmail.com>
2015-02-21 15:57:59 +0200

    working on server packaging

--- webz-server-wrappers/src/main/java/org/terems/WebzLauncher.java
+++ webz-server-wrappers/src/main/java/org/terems/WebzLauncher.java
@@ -1,3 +1,21 @@
+/*
+ * WebZ Server can serve web pages from various local and remote file sources.
+ * Copyright (C) 2014-2015  Oleksandr Tereschenko <http://www.terems.org/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.terems;
 
 import java.io.File;
@@ -8,25 +26,23 @@ public class WebzLauncher {
 
 	public static void main(String[] args) throws Exception {
 
-		File webzWar = new File("webz.war");
-		if (!webzWar.exists()) {
+		String appHome = System.getProperty("app.home");
 
-			File upperWebzWar = new File("../webz.war");
-			if (upperWebzWar.exists()) {
-				webzWar = upperWebzWar;
-			}
-		}
+		// TODO get rid of war name hardcode:
+		String webzWarName = appHome + "/lib/webz-0.9.1-SNAPSHOT.war";
+		File webzWar = new File(webzWarName);
 
 		Tomcat tomcat = new Tomcat();
+		tomcat.setBaseDir(appHome + "/temp");
+		tomcat.setSilent(true);
 
-		// The port that we should run on can be set into an environment variable
-		// Look for that variable and default to 8887 if it isn't there.
-		String webPort = System.getenv("PORT");
-		if (webPort == null || webPort.isEmpty()) {
-			webPort = "8887";
+		String port = System.getenv("PORT");
+		if (port == null || port.isEmpty()) {
+			port = "8887";
 		}
+		// TODO make WebZ Server log port number instead of Tomcat
 
-		tomcat.setPort(Integer.valueOf(webPort));
+		tomcat.setPort(Integer.valueOf(port));
 
 		tomcat.addWebapp("/", webzWar.getAbsolutePath());
 		System.out.println("Deploying WebZ from: " + webzWar);
