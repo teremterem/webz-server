2d8bf09235c13739bc3a891216d945d2d7dc8375
teremterem <toporok@gmail.com>
2015-02-24 14:48:01 +0200

    added proper "favicons" for the GUI part

--- webz-server-packager/src/main/java/org/terems/HelloFromWebZ.java
+++ webz-server-packager/src/main/java/org/terems/HelloFromWebZ.java
@@ -5,9 +5,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -20,20 +24,18 @@ import org.apache.tomcat.util.http.fileupload.IOUtils;
 
 public class HelloFromWebZ {
 
+	private static final String[] ICON_RESOURCE_NAMES = { "/favicon16.png", "/favicon32.png", "/favicon64.png", "/favicon128.png",
+			"/favicon256.png", "/favicon512.png", "/favicon1024.png" };
+
 	private static void createAndShowMainWindow() {
 
 		JFrame frame = new JFrame("WebZ Server v0.9 beta (Pedesis)");
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		ByteArrayOutputStream faviconStream = new ByteArrayOutputStream();
-		try {
-			IOUtils.copy(HelloFromWebZ.class.getResourceAsStream("/favicon.png"), faviconStream);
-
-			ImageIcon img = new ImageIcon(faviconStream.toByteArray());
-			frame.setIconImage(img.getImage());
-		} catch (IOException e) {
-			// it's just a favicon - ignore
+		List<Image> favicons = fetchFavicons();
+		if (!favicons.isEmpty()) {
+			frame.setIconImages(favicons);
 		}
 
 		JPanel panel = new JPanel();
@@ -64,6 +66,28 @@ public class HelloFromWebZ {
 		frame.setLocation(x, y);
 
 		frame.setVisible(true);
+	}
+
+	private static List<Image> fetchFavicons() {
+
+		List<Image> favicons = new ArrayList<Image>(ICON_RESOURCE_NAMES.length);
+
+		ByteArrayOutputStream faviconOut = new ByteArrayOutputStream();
+		for (String iconResourceName : ICON_RESOURCE_NAMES) {
+			try {
+				faviconOut.reset();
+
+				InputStream resourceIn = HelloFromWebZ.class.getResourceAsStream(iconResourceName);
+				if (resourceIn != null) {
+
+					IOUtils.copy(resourceIn, faviconOut);
+					favicons.add(new ImageIcon(faviconOut.toByteArray()).getImage());
+				}
+			} catch (IOException e) {
+				// it's just a favicon - ignore
+			}
+		}
+		return favicons;
 	}
 
 	public static void main(String[] args) {
